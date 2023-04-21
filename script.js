6468ee27d1b0b8c73d62dd5d4dc96f67ea12af5c
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-04-21 12:55:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-function volume_sphere() {
-    //Write your code here
+function volume_sphere(e) {
+//Write your code here
+e.preventDefault()
+const radius = document.querySelector('#radius')
+const volume = document.querySelector('#volume')
+const submitBtn = document.querySelector('#submit')
 
-	const radius = document.querySelector('#radius')
-	const volume = document.querySelector('#volume')
-	const submitBtn = document.querySelector('#submit')
+submitBtn.addEventListener('click', function () {
+const result = eval(radius.value radius.value 3.14)
+volume.value = result
+})
 
-	submitBtn.addEventListener('click', function () {
-		const result = eval(radius.value * radius.value * 3.14)
-		volume.value = result
-	})
-
-	function cal (e){
-		e.preventDefault()
-	}
-} 
+}
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
